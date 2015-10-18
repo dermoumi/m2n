1ca4f04145206b7077fec4bffb4a341989b85a70
Dermoumi S <sdermoumi@gmail.com>
2015-10-19 06:47:47 +0800

    Fixed error when pushing class Timer through a LuaVM

--- bin/assets/scripts/nx/timer.lua
+++ bin/assets/scripts/nx/timer.lua
@@ -1,16 +1,17 @@
-local ffi = require 'ffi'
 local class = require 'nx.class'
 local Nx = require 'nx'
 
 local Timer = class 'nx.timer'
 
 function Timer.static._fromCData(data)
+    local ffi = require 'ffi'
     local timer = Timer:allocate()
-    timer._cdata = data
+    timer._cdata = ffi.cast('double*', data)
     return timer
 end
 
 function Timer:initialize()
+    local ffi = require 'ffi'
     self._cdata = ffi.new('double[1]', {Nx.getSystemTime()})
 end
 
