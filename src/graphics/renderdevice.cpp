d64aa5163765d6f74ec07cf7b7dee7a4464f4c9a
Dermoumi S <sdermoumi@gmail.com>
2015-11-14 13:08:27 +0000

    RenderDevice now correctly calculates PVRTC and ETC1 texture sizes

--- src/graphics/renderdevice.cpp
+++ src/graphics/renderdevice.cpp
@@ -43,6 +43,14 @@ uint32_t RenderDevice::calcTextureSize(TextureFormat format, int width, int heig
         return width * height * depth * 8;
     case RGBA32F:
         return width * height * depth * 16;
+    case PVRTCI_2BPP:
+    case PVRTCI_A2BPP:
+        return (std::max( width, 16 ) * std::max( height, 8 ) * 2 + 7) / 8;
+    case PVRTCI_4BPP:
+    case PVRTCI_A4BPP:
+        return (std::max( width, 8 ) * std::max( height, 8 ) * 4 + 7) / 8;
+    case ETC1:
+        return std::max( width / 4, 1 ) * std::max( height / 4, 1 ) * depth * 8;
     default:
         return 0;
     }
