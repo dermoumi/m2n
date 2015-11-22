5b408e4c7b8f25944fff9552fa8a092e5c38fd0a
Dermoumi S <sdermoumi@gmail.com>
2015-11-22 20:15:09 +0800

    Implemented a bunch of Audio functionality

--- bin/assets/scripts/nx/audio.lua
+++ bin/assets/scripts/nx/audio.lua
@@ -34,6 +34,27 @@ local C = ffi.C
 ffi.cdef [[
     bool nxAudioInit();
     void nxAudioRelease();
+    void nxAudioStopAll();
+    uint32_t nxAudioActiveVoiceCount();
+    uint32_t nxAudioVoiceCount();
+    float nxAudioPostClipScaler();
+    float nxAudioGlobalVolume();
+    uint32_t nxAudioMaxActiveVoiceCount();
+    void nxAudioSetMaxActiveVoiceCount(uint32_t);
+    void nxAudioSetGlobalVolume(float);
+    void nxAudioPauseAll(bool);
+    void nxAudioFadeGlobalVolume(float, double);
+    void nxAudioSetVisualizationEnabled(bool);
+    const float* nxAudioCurrentWaveData();
+    void nxAudioUpdate3dAudio();
+    void nxAudioSet3dSoundSpeed(float);
+    float nxAudio3dSoundSpeed();
+    void nxAudioSet3dListenerParameters(float, float, float, float, float, float, float, float, float,
+        float, float, float);
+    void nxAudioSet3dListenerPosition(float, float, float);
+    void nxAudioSet3dListenerAt(float, float, float);
+    void nxAudioSet3dListenerUp(float, float, float);
+    void nxAudioSet3dListenerVelocity(float, float, float);
 ]]
 
 ------------------------------------------------------------
@@ -47,6 +68,108 @@ end
 ------------------------------------------------------------
 function Audio.release()
     C.nxAudioRelease()
+end
+
+------------------------------------------------------------
+function Audio.stopAll()
+    C.nxAudioStopAll()
+end
+
+------------------------------------------------------------
+function Audio.pauseAll(pause)
+    C.nxAudioPauseAll(pause)
+end
+
+------------------------------------------------------------
+function Audio.setGlobalVolume(volume)
+    C.nxAudioSetGlobalVolume(volume)
+end
+
+------------------------------------------------------------
+function Audio.setMaxActiveVoiceCount(count)
+    C.nxAudioSetMaxActiveVoiceCount(count)
+end
+
+------------------------------------------------------------
+function Audio.setVisualizationEnabled(enabled)
+    C.nxAudioSetVisualizationEnabled(enabled)
+end
+
+------------------------------------------------------------
+function Audio.update3d()
+    C.nxAudioUpdate3dAudio()
+end
+
+------------------------------------------------------------
+function Audio.setSoundSpeed(speed)
+    C.nxAudioSet3dSoundSpeed(speed)
+end
+
+------------------------------------------------------------
+function Audio.setListenerParameters(
+    posX, posY, posZ, atX, atY, atZ, upX, upY, upZ, velX, velY, velZ
+)
+    C.nxAudioSet3dListenerParameters(posX, posY, posZ, atX, atY, atZ, upX, upY, upZ, velX or 0,
+        velY or 0, velZ or 0)
+end
+
+------------------------------------------------------------
+function Audio.setListenerPosition(x, y, z)
+    C.nxAudioSet3dListenerPosition(x, y, z)
+end
+
+------------------------------------------------------------
+function Audio.setListenerAt(x, y, z)
+    C.nxAudioSet3dListenerAt(x, y, z)
+end
+
+------------------------------------------------------------
+function Audio.setListenerUp(x, y, z)
+    C.nxAudioSet3dListenerUp(x, y, z)
+end
+
+------------------------------------------------------------
+function Audio.setListenerVelocity(x, y, z)
+    C.nxAudioSet3dListenerVelocity(x, y, z)
+end
+
+------------------------------------------------------------
+function Audio.voiceCount(active)
+    if active then
+        return C.nxAudioActiveVoiceCount()
+    else
+        return C.nxAudioVoiceCount()
+    end
+end
+
+------------------------------------------------------------
+function Audio.postClipScaler()
+    return C.nxAudioPostClipScaler()
+end
+
+------------------------------------------------------------
+function Audio.globalVolume()
+    return C.nxAudioGlobalVolume()
+end
+
+------------------------------------------------------------
+function Audio.maxActiveVoiceCount()
+    return C.nxAudioMaxActiveVoiceCount()
+end
+
+------------------------------------------------------------
+function Audio.currentWaveData()
+    return C.nxAudioCurrentWaveData()
+end
+
+------------------------------------------------------------
+function Audio.soundSpeed()
+    return C.nxAudio3dSoundSpeed()
+end
+
+------------------------------------------------------------
+function Audio.fadeGlobalVolume(to, time)
+    C.nxAudioFadeGlobalVolume(to, time)
 end
 
 ------------------------------------------------------------
